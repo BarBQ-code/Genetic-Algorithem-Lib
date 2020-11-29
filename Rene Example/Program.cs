e1658becbf0ed05e1c9fef0930bdd11e39ec3c14
BarBQ-code <74874711+BarBQ-code@users.noreply.github.com>
2020-11-29 13:14:12 +0200

    Update Program.cs

--- Rene Example/Program.cs
+++ Rene Example/Program.cs
@@ -1,11 +1,6 @@
 ﻿using GeneticLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Rene_Example
 {
